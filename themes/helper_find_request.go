3b533169486a8341f78ca84d21d3447dce460d5a
Paweł <wolacinio@gmail.com>
2018-02-23 14:57:29 +0100

    Update helper_find_request.go

--- themes/helper_find_request.go
+++ themes/helper_find_request.go
@@ -2,7 +2,7 @@ package themes
 
 import "github.com/wolacinio/postmanerator/postman"
 
-func helperFindRequest(req postman.Request, name string) *postman.Request {
+func helperFindRequest(req postman.Request, name string) *postman.Response {
 	for _, res := range req.Request {
 			return &res
 	}
